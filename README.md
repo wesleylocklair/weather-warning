ed087b2b01bd1956d67ede1e6b10c76d5aef8d56
Boss Eggs <wesley.j.locklair@gmail.com>
2024-08-20 19:00:21 -0400

    The search works not les goo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# weather-forecast
+# Weather-Warning
 ## Description
 In this project I am in the works of building a weather application. You will be able to put in any city and see the weather for the next 5 days. As of rightnow only the current weather in Charlotte NC is logged in the console when you press submit. I have not figured out how to use the input box html element to replace the location in the url. Going to mess with it more in the future.
 ## Credits
@@ -13,5 +13,5 @@ Here is the link to the webpage -
 https://wesleylocklair.github.io/weather-forecast/
 
 ## Screenshots
-![Screenshot (9)](https://github.com/wesleylocklair/weather-forecast/assets/171185367/81b68133-5076-4927-b537-7b49ec021b67)
+
 
--- scipt.js
+++ scipt.js
@@ -2,15 +2,13 @@ const city = document.getElementById('cityinput')
 const typedcity = city.value
 const citybtn = document.getElementById('citysubmit')
 const apikey = "3f0aa8533877eb317a4dac0b065b71a7"
-const cityrequest = `https://api.openweathermap.org/data/2.5/forecast?q=Charlotte,us&appid=3f0aa8533877eb317a4dac0b065b71a7`
 const results = document.getElementsByClassName('info')
 
 
 
 citybtn.addEventListener('click', (event) => {
- 
+  const cityrequest = `https://api.openweathermap.org/data/2.5/forecast?q=${city.value},us&appid=3f0aa8533877eb317a4dac0b065b71a7`
     event.preventDefault
-    console.log(city.value);
 
     fetch(cityrequest)
     .then(function  (response) {
@@ -22,3 +20,8 @@ citybtn.addEventListener('click', (event) => {
         console.log(data.list[0].main)
       });
    })
+
+   // I need city name, date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+   //THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+   //WHEN I click on a city in the search history
+  //THEN I am again presented with current and future conditions for that city
